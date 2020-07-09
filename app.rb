7cf1a73d41bc517a6e0c2e06e4b5709f2c5a0577
Mahmoud Bakr <mbakr6821@gmail.com>
2020-07-09 15:07:18 +0200

    multiply_els, my_map_variable to use Proc

--- app.rb
+++ app.rb
@@ -64,10 +64,14 @@ module Enumerable
     counting
   end
 
-  def my_map
+  def my_map(proc = nil)
     arr = []
     for i in self
-      arr.push(yield i)
+      if (proc != nil)
+        arr.push(proc)
+      else
+        arr.push(yield i)
+      end
     end
     arr
   end
@@ -90,14 +94,22 @@ module Enumerable
   end
 end
 
-numbers = [1,2,3,4,5]
-# numbers.my_each {|number| puts "current number is #{number}"}
-# numbers.my_each_with_index {|number, index| puts "The value is #{number} and the index is #{index}"}
-# p numbers.my_select {|number| number > 2}
-# p numbers.my_all? {|number| number > 0}
-# p numbers.my_any? {|number| number > 1}
-# p numbers.my_none? {|number| number < 2}
-# p numbers.my_count(7)
-# p numbers.my_map { |n| n * 2 }
+def multiply_els(arr)
+  arr.my_inject { |product, n| product * n }
+end
 
-p numbers.my_inject(1) { |product, n| product * n }
+numbers = [1,2,3,4,5]
+numbers.my_each {|number| puts "current number is #{number}"}
+numbers.my_each_with_index {|number, index| puts "The value is #{number} and the index is #{index}"}
+p numbers.my_select {|number| number > 2}
+p numbers.my_all? {|number| number > 0}
+p numbers.my_any? {|number| number > 1}
+p numbers.my_none? {|number| number < 2}
+p numbers.my_count(6)
+p numbers.my_map { |n| n + 2 }
+p numbers.my_inject(1) { |product, n| product + n }
+p multiply_els([2,4,5])
+
+my_map_variable = Proc.new { |n| n * 2 }
+p numbers.my_map(&my_map_variable)
+
