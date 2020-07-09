717aab80ba7db883b5b2eeb23e9b034374440b7a
unknown <nusretbutunay@gmail.com>
2020-07-09 15:30:04 +0300

    complete my_inject method

--- app.rb
+++ app.rb
@@ -71,14 +71,33 @@ module Enumerable
     end
     arr
   end
+
+  def my_inject(arg=nil)
+    product = 0
+    i = 0
+    while i < self.length
+      product = yield product, self[i]
+      if(product == 0)
+        product=1
+        product = yield product, self[i]
+      end
+      i+=1
+    end
+    if arg !=nil
+      product = yield product, arg
+    end
+    product
+  end
 end
 
-numbers = [4,1,3,2,6,7,1,7,7]
-numbers.my_each {|number| puts "current number is #{number}"}
-numbers.my_each_with_index {|number, index| puts "The value is #{number} and the index is #{index}"}
-p numbers.my_select {|number| number > 2}
-p numbers.my_all? {|number| number > 0}
-p numbers.my_any? {|number| number > 1}
-p numbers.my_none? {|number| number < 2}
-p numbers.my_count(7)
-p numbers.my_map { |n| n * 2 }
+numbers = [1,2,3,4,5]
+# numbers.my_each {|number| puts "current number is #{number}"}
+# numbers.my_each_with_index {|number, index| puts "The value is #{number} and the index is #{index}"}
+# p numbers.my_select {|number| number > 2}
+# p numbers.my_all? {|number| number > 0}
+# p numbers.my_any? {|number| number > 1}
+# p numbers.my_none? {|number| number < 2}
+# p numbers.my_count(7)
+# p numbers.my_map { |n| n * 2 }
+
+p numbers.my_inject(1) { |product, n| product * n }
