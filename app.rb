a5fc52667c3e236f04ad16d39a5943f84a4fcace
Mahmoud Bakr <mbakr6821@gmail.com>
2020-07-10 12:59:51 +0200

    run rubocop command line

--- app.rb
+++ app.rb
@@ -1,9 +1,10 @@
 module Enumerable
   def my_each
     return enum_for unless block_given?
+
     i = 0
-    new_array = self.is_a?(Range) ? self.to_a : self
-    new_array = self.is_a?(Hash) ? self.to_a : self
+    new_array = is_a?(Range) ? to_a : self
+    new_array = is_a?(Hash) ? to_a : self
     while i < new_array.length
       yield new_array[i]
       i += 1
@@ -13,9 +14,10 @@ module Enumerable
 
   def my_each_with_index
     return enum_for unless block_given?
+
     i = 0
     # new_array = self.is_a?(Range) ? self.to_a : self
-    new_array = self.is_a?(Hash) ? self.to_a : self
+    new_array = is_a?(Hash) ? to_a : self
     while i < new_array.length
       yield new_array[i], i
       i += 1
@@ -25,6 +27,7 @@ module Enumerable
 
   def my_select
     return enum_for unless block_given?
+
     new_numbers = []
     each do |i|
       new_numbers.push(i) if yield(i)
@@ -34,7 +37,7 @@ module Enumerable
 
   def my_all?(arg)
     condition = true
-    if arg != nil
+    unless arg.nil?
       each do |j|
         condition = false unless j.class == arg || arg.match(j)
       end
@@ -44,9 +47,9 @@ module Enumerable
         condition = false unless j
       end
     end
-    if block_given? 
+    if block_given?
       each do |j|
-      condition = false unless yield(j)
+        condition = false unless yield(j)
       end
     end
     condition
@@ -54,9 +57,9 @@ module Enumerable
 
   def my_any?(arg = nil)
     condition = false
-    if arg != nil
+    unless arg.nil?
       each do |j|
-        condition = true if j.class == arg #|| arg.match(j)
+        condition = true if j.class == arg # || arg.match(j)
       end
     end
     unless block_given?
@@ -64,41 +67,42 @@ module Enumerable
         condition = true if j
       end
     end
-    if block_given? 
+    if block_given?
       each do |j|
-      condition = true if yield(j)
+        condition = true if yield(j)
       end
     end
     condition
   end
 
   def my_none?(arg = nil)
-    !self.my_any?(arg)
+    !my_any?(arg)
   end
 
   def my_count(counted = nil)
     j = 0
-    new_array = self.is_a?(Range) ? self.to_a : self
-    if counted == nil && !block_given?
-      return new_array.length
-    end
+    new_array = is_a?(Range) ? to_a : self
+    return new_array.length if counted.nil? && !block_given?
+
     counting = 0
     if block_given?
       while j < new_array.length
-        counting+=1 if yield new_array[j]
-        j+=1
+        counting += 1 if yield new_array[j]
+        j += 1
       end
   end
-    unless counted == nil
+    unless counted.nil?
       while j < new_array.length
-        counting+=1 if new_array[j] == counted
-        j+=1
+        counting += 1 if new_array[j] == counted
+        j += 1
       end
     end
     counting
   end
 
   def my_map(proc = nil)
+    return enum_for unless block_given?
+
     arr = []
     each do |i|
       if !proc.nil?
@@ -110,18 +114,39 @@ module Enumerable
     arr
   end
 
-  def my_inject(arg = nil)
+  def my_inject(*arg)
+    new_array = is_a?(Range) ? to_a : self
     product = 0
     i = 0
-    while i < length
-      product = yield product, self[i]
-      if product.zero?
-        product = 1
-        product = yield product, self[i]
+    if !block_given?
+      if arg[0].class == Symbol
+        while i < new_array.length
+          product = product.send(arg[0], new_array[i])
+          if product.zero?
+            product = 1
+            product = product.send(arg[0], new_array[i])
+          end
+          i += 1
+        end
+      elsif arg[0].class != Symbol && !arg[1].nil?
+        while i < new_array.length
+          product = product.send(arg[1], new_array[i])
+          i += 1
+          product = 1 if product.zero?
+        end
+        product = product.send(arg[1], arg[0])
       end
-      i += 1
+    else
+      while i < new_array.length
+        product = yield product, new_array[i]
+        if product.zero?
+          product = 1
+          product = yield product, new_array[i]
+        end
+        i += 1
+      end
+      product = yield product, arg unless arg.nil?
     end
-    product = yield product, arg unless arg.nil?
     product
   end
 end
@@ -143,3 +168,8 @@ end
 # p (1..5).my_count
 # p [1, 2, 3, 4, 5].my_count {|num| num > 3}
 # p [1, 2, 3].count{ |num| num + 1 }
+# p [1, 2, 3].my_map
+# p (1..5).my_inject { |prod, n| prod * n }
+# p (1..5).my_inject(2) { |prod, n| prod * n }
+# p [1, 2, 3].my_inject(:+)
+# p (1..5).my_inject(3, :+)
