79bc6ac07da24b6c84cc5434da92d31a8735aa75
unknown <nusretbutunay@gmail.com>
2020-07-10 17:19:19 +0300

    make changes for my_inject

--- app.rb
+++ app.rb
@@ -3,8 +3,8 @@ module Enumerable
     return enum_for unless block_given?
 
     i = 0
-    new_array = is_a?(Range) ? to_a : self
-    new_array = is_a?(Hash) ? to_a : self
+    new_array = self
+    new_array = to_a if is_a?(Range) || is_a?(Hash)
     while i < new_array.length
       yield new_array[i]
       i += 1
@@ -16,8 +16,8 @@ module Enumerable
     return enum_for unless block_given?
 
     i = 0
-    # new_array = self.is_a?(Range) ? self.to_a : self
-    new_array = is_a?(Hash) ? to_a : self
+    new_array = self
+    new_array = to_a if is_a?(Range) || is_a?(Hash)
     while i < new_array.length
       yield new_array[i], i
       i += 1
@@ -29,50 +29,27 @@ module Enumerable
     return enum_for unless block_given?
 
     new_numbers = []
-    each do |i|
-      new_numbers.push(i) if yield(i)
-    end
+    my_each { |i| new_numbers.push(i) if yield(i) }
     new_numbers
   end
 
-  def my_all?(arg)
-    condition = true
+  def my_all?(arg = nil)
+    return false if arg.nil? && !block_given? && !my_each { |j| j }
+
     unless arg.nil?
-      each do |j|
-        condition = false unless j.class == arg || arg.match(j)
-      end
+      return false unless my_each { |j| j.class == arg || arg.match(j) }
     end
-    unless block_given?
-      each do |j|
-        condition = false unless j
-      end
-    end
-    if block_given?
-      each do |j|
-        condition = false unless yield(j)
-      end
-    end
-    condition
+    return false if block_given? && !my_each { |j| yield(j) }
+
+    true
   end
 
   def my_any?(arg = nil)
-    condition = false
-    unless arg.nil?
-      each do |j|
-        condition = true if j.class == arg # || arg.match(j)
-      end
-    end
-    unless block_given?
-      each do |j|
-        condition = true if j
-      end
-    end
-    if block_given?
-      each do |j|
-        condition = true if yield(j)
-      end
-    end
-    condition
+    return true unless arg.nil? || !my_each { |j| j.class == arg }
+    return true unless block_given? || !my_each { |j| j }
+    return true if block_given? && my_each { |j| yield(j) }
+
+    true
   end
 
   def my_none?(arg = nil)
@@ -80,22 +57,15 @@ module Enumerable
   end
 
   def my_count(counted = nil)
-    j = 0
     new_array = is_a?(Range) ? to_a : self
     return new_array.length if counted.nil? && !block_given?
 
     counting = 0
     if block_given?
-      while j < new_array.length
-        counting += 1 if yield new_array[j]
-        j += 1
-      end
-  end
+      new_array.my_each { |j| counting += 1 if yield j }
+    end
     unless counted.nil?
-      while j < new_array.length
-        counting += 1 if new_array[j] == counted
-        j += 1
-      end
+      new_array.my_each { |j| counting += 1 if j == counted }
     end
     counting
   end
@@ -117,35 +87,28 @@ module Enumerable
   def my_inject(*arg)
     new_array = is_a?(Range) ? to_a : self
     product = 0
-    i = 0
     if !block_given?
       if arg[0].class == Symbol
-        while i < new_array.length
-          product = product.send(arg[0], new_array[i])
-          if product.zero?
-            product = 1
-            product = product.send(arg[0], new_array[i])
-          end
-          i += 1
+        new_array.my_each { |i| product = product.send(arg[0], i) }
+        if product.zero?
+          product = 1
+          new_array.my_each { |i| product = product.send(arg[0], i) }
         end
       elsif arg[0].class != Symbol && !arg[1].nil?
-        while i < new_array.length
-          product = product.send(arg[1], new_array[i])
-          i += 1
-          product = 1 if product.zero?
+        new_array.my_each { |i| product = product.send(arg[1], i) }
+        if product.zero?
+          product = 1
+          new_array.my_each { |i| product = product.send(arg[1], i) }
         end
         product = product.send(arg[1], arg[0])
       end
     else
-      while i < new_array.length
-        product = yield product, new_array[i]
-        if product.zero?
-          product = 1
-          product = yield product, new_array[i]
-        end
-        i += 1
+      new_array.my_each { |i| product = yield product, i }
+      if product.zero?
+        product = 1
+        new_array.my_each { |i| product = yield product, i }
       end
-      product = yield product, arg unless arg.nil?
+      product = yield product, arg[0] unless arg[0].nil?
     end
     product
   end
@@ -154,22 +117,3 @@ end
 def multiply_els(arr)
   arr.my_inject { |product, n| product * n }
 end
-
-# p [1, 2, 3].my_select
-# p [1, true, 'hi', []].my_all?
-# p [1.0, 2.0, 3.0].my_all?(Float)
-# p ['dog','door','dish'].my_all?(/d/)
-# p [1, 'dog' , 1].my_any?
-# p ['a', 'b', 3].my_none?(Integer)
-# p ['b', 2i, 'a'].my_none?(Numeric)
-# p ['dog','door','dish'].my_none?(/s/)
-# p ['dog','door','dish'].my_none?(/dog/)
-# p [1, 2, 3].my_count(3)
-# p (1..5).my_count
-# p [1, 2, 3, 4, 5].my_count {|num| num > 3}
-# p [1, 2, 3].count{ |num| num + 1 }
-# p [1, 2, 3].my_map
-# p (1..5).my_inject { |prod, n| prod * n }
-# p (1..5).my_inject(2) { |prod, n| prod * n }
-# p [1, 2, 3].my_inject(:+)
-# p (1..5).my_inject(3, :+)
