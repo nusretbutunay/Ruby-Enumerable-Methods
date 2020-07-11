a6812713b3194606a94c1ca7f73417d493753378
Nusret Bütünay <nusretbutunay@gmail.com>
2020-07-11 19:41:40 +0300

    fix my_each_with_index and my_inject

--- app.rb
+++ app.rb
@@ -24,10 +24,10 @@ module Enumerable
     return enum_for unless block_given?
 
     i = 0
-    j = 1
     new_array = self
     new_array = to_a if is_a?(Hash)
     if is_a?(Range)
+      j = first
       step(j).each do |n|
         yield n, j
         j += 1
@@ -145,29 +145,40 @@ module Enumerable
   def my_inject(*arg)
     new_array = is_a?(Range) ? to_a : self
     product = 0
+    long_word = ''
+    i = 0
+    temp = new_array[0]
+    while i < new_array.length - 1
+      long_word = yield temp, new_array[i + 1] if new_array[i].is_a?(String)
+      i += 1
+    end
+    return long_word if long_word != ''
+
     if !block_given?
       if arg[0].class == Symbol
-        new_array.my_each { |i| product = product.send(arg[0], i) }
+        new_array.my_each { |j| product = product.send(arg[0], j) }
         if product.zero?
           product = 1
-          new_array.my_each { |i| product = product.send(arg[0], i) }
+          new_array.my_each { |j| product = product.send(arg[0], j) }
         end
       elsif arg[0].class != Symbol && !arg[1].nil?
-        new_array.my_each { |i| product = product.send(arg[1], i) }
+        new_array.my_each { |j| product = product.send(arg[1], j) }
         if product.zero?
           product = 1
-          new_array.my_each { |i| product = product.send(arg[1], i) }
+          new_array.my_each { |j| product = product.send(arg[1], j) }
         end
         product = product.send(arg[1], arg[0])
       end
     else
-      new_array.my_each { |i| product = yield product, i }
+      new_array.my_each { |j| product = yield product, j }
       if product.zero?
         product = 1
-        new_array.my_each { |i| product = yield product, i }
+        new_array.my_each { |j| product = yield product, j }
       end
       product = yield product, arg[0] unless arg[0].nil?
     end
+    raise LocalJumpError unless block_given?
+
     product
   end
 end
