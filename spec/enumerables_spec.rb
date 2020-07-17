423b69a5fee731c09b1482aafdde4c2d6a0a40c5
unknown <nusretbutunay@gmail.com>
2020-07-17 12:16:05 +0300

    complete my_select

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -1,9 +1,10 @@
 require './app'
 describe Enumerable do
   describe "#my_select" do
-   let(:arr) {[3, 2, 4, 5, 7, 9, 12, 43, 23]}
-   it 'iterates through the array and returns the satisfying items in the array' do
-    expect { |b| arr.my_select(&b) }.to yield_control
-   end
+    let(:block) { proc { |num| num < 8 } }
+    let(:arr) {[3, 2, 4, 5, 7, 9, 12, 43, 23]}
+    it 'iterates through the array and returns the satisfying items in the array' do
+      expect(arr.my_select(&block)).to eql([3,2,4,5,7])
+    end
   end
 end
